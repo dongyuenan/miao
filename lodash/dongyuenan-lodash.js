e52c451f4116da251e5c322d1802df53874dd171
dyn <1579280176@qq.com>
2022-09-14 19:37:00 +0800

    lodash

--- lodash/dongyuenan-lodash.js
+++ lodash/dongyuenan-lodash.js
@@ -1,3 +1,4 @@
+
 var dongyuenan = {
   chunk: function (array, size = 1) {
     var res = []
@@ -19,5 +20,58 @@ var dongyuenan = {
       res.push(tem)
     }
     return res
-  }
+  },
+
+  compact: function (array) {
+    var res = []
+    for (var i = 0; i < array.length; i++) {
+      if (array[i]) {
+        res.push(array[i])
+      }
+    }
+    return res
+  },
+
+  concat: function (array, ...val) {
+    var value = [...val]
+    for (var i = 0; i < value.length; i++) {
+      if (Array.isArray(value[i])) {
+        array.push(...value[i])
+      } else {
+        array.push(value[i])
+      }
+    }
+    return array
+  },
+
+  difference: function (ary, val) {
+    var res = []
+    for (var i = 0; i < ary.length; i++) {
+      for (var j = 0; j < val.length; ) {
+        var a = ary[i]
+        var b = val[j]
+        if (a != b) {
+          j++
+        } else {
+          break
+        }
+      }
+      res.push(a)
+    }
+    return res
+  },
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
