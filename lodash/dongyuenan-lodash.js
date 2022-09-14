b05f33d7cd88e0c5eb06af74deb4b72b23ba3233
dyn <1579280176@qq.com>
2022-09-14 22:29:07 +0800

    lodash

--- lodash/dongyuenan-lodash.js
+++ lodash/dongyuenan-lodash.js
@@ -1,72 +1,247 @@
 
 var dongyuenan = {
-  chunk: function (array, size = 1) {
-    var res = []
-    var tem = []
-    if (size == 0) {    // 如果size为0直接返回空数组
-      return res
-    }
-    if (size == array.length) {    // 如果size等于原数组长度返回原数组
-      return array
-    }
-    for (var i = 0; i < array.length; i++) {
-      tem.push(array[i])
-      if (tem.length == size) {
-        res.push(tem)
-        tem = []
-      }
-    }
-    if (tem[0]) {     //最后只要tem里面还有元素就需要把tem放进res里面
-      res.push(tem)
-    }
-    return res
-  },
 
-  compact: function (array) {
-    var res = []
-    for (var i = 0; i < array.length; i++) {
-      if (array[i]) {
-        res.push(array[i])
+  chunk:
+    function (array, size = 1) {
+      var res = []
+      var tem = []
+      if (size == 0) {    // 如果size为0直接返回空数组
+        return res
       }
-    }
-    return res
-  },
-
-  concat: function (array, ...val) {
-    var value = [...val]
-    for (var i = 0; i < value.length; i++) {
-      if (Array.isArray(value[i])) {
-        array.push(...value[i])
-      } else {
-        array.push(value[i])
+      if (size == array.length) {    // 如果size等于原数组长度返回原数组
+        return array
       }
-    }
-    return array
-  },
-
-  difference: function (ary, val) {
-    var res = []
-    for (var i = 0; i < ary.length; i++) {
-      for (var j = 0; j < val.length; ) {
-        var a = ary[i]
-        var b = val[j]
-        if (a != b) {
-          j++
-        } else {
-          break
+      for (var i = 0; i < array.length; i++) {
+        tem.push(array[i])
+        if (tem.length == size) {
+          res.push(tem)
+          tem = []
         }
       }
-      res.push(a)
+      if (tem[0]) {     //最后只要tem里面还有元素就需要把tem放进res里面
+        res.push(tem)
+      }
+      return res
+    },
+
+  compact:
+    function (array) {
+      var res = []
+      for (var i = 0; i < array.length; i++) {
+        if (array[i]) {
+          res.push(array[i])
+        }
+      }
+      return res
+    },
+
+  concat:
+    function (array, ...val) {
+      var value = [...val]
+      for (var i = 0; i < value.length; i++) {
+        if (Array.isArray(value[i])) {
+          array.push(...value[i])
+        } else {
+          array.push(value[i])
+        }
+      }
+      return array
+    },
+
+  difference:
+    function (ary, val) {
+      var res = []
+      for (var i = 0; i < ary.length; i++) {
+        for (var j = 0; j < val.length;) {
+          var a = ary[i]
+          var b = val[j]
+          if (a != b) {
+            j++
+          } else {
+            break
+          }
+        }
+        res.push(a)
+      }
+      return res
+    },
+
+  differenceBy:
+    function () {
+
+    },
+
+  differenceWith:
+    function () {
+
+    },
+
+  drop:
+    function (ary, n = 1) {
+      var res = []
+      for (var i = n; i < ary.length; i++) {
+        res.push(ary[i])
+      }
+      return res
+    },
+
+  dropRight:
+    function (ary, n = 1) {
+      var res = []
+      for (var i = 0; i < ary.length - n; i++) {
+        res.push(ary[i])
+      }
+      return res
+    },
+
+  dropRightWhile:
+    function (ary, predicate) {
+
+    },
+
+  dropWhile:
+    function (ary, predicate) {
+
+    },
+
+  fill:
+    function (ary, value, start = 0, end = ary.length) {
+      for (var i = start; i < end; i++) {
+        ary[i] = value
+      }
+      return ary
+    },
+
+  findIndex:
+    function (ary, predicate) {
+      for (var i = 0; i < ary.length; i++) {
+        if (predicate(ary[i])) {
+          return i
+        }
+      }
+      return -1
+    },
+
+  findLastIndex:
+    function (ary, predicate) {
+      for (var i = ary.length - 1; i > 0; i--) {
+        if (predicate(ary[i])) {
+          return i
+        }
+      }
+      return -1
+    },
+
+  flatten:     /**减少一级嵌套深度 */
+    function (ary) {
+      var res = []
+      for (var i = 0; i < ary.length; i++) {
+        if (Array.isArray(ary[i])) {
+          var item = ary[i]
+          var len = item.length
+          for (var j = 0; j < len; j++) {
+            res.push(item[j])
+          }
+        } else {
+          res.push(ary[i])
+        }
+      }
+      return res
+    },
+
+  flattenDeep:    /**递归为一维数组 */
+    function (ary) {
+      var res = []
+      var hasArrayInArray = false   //标记array中还有没有数组
+      for (var i = 0; i < ary.length; i++) {
+
+      }
+    },
+
+  flattenDepth:   /**根据 depth 递归减少 array 的嵌套层级 */
+    function (ary, depth = 1) {
+      for (var k = 0; k < depth; k++) {
+        //从这
+        var res = []
+        var hasArrayInArray = false   //标记array中还有没有数组
+        for (var i = 0; i < ary.length; i++) {
+          var item = ary[i]
+          if (Array.isArray(item)) {
+            hasArrayInArray = true
+            for (var j = 0; j < item.length; j++) {
+              res.push(item[j])
+            }
+          } else {
+            res.push(item)
+          }
+        }
+        if (!hasArrayInArray) {  //如果array中不再有数组，则可以不继续降维了
+          break
+        }
+        //到这，会把array降低一个维度，并将结果放入result上
+        ary = res
+      }
+      return res
+    },
+
+  fromPairs:
+    function (pairs) {
+      var obj = {}
+      for (var i = 0; i < pairs.length; i++) {
+        var item = pairs[i][0]
+        obj[item] = pairs[i][1]
+      }
+      return obj
+    },
+
+  head:
+    function (ary) {
+      return ary[0]
+    },
+
+  indexOf:
+    function (ary, value, fromIndex = 0) {
+      if (fromIndex >= 0) {
+        for (var i = fromIndex; i < ary.length; i++) {
+          if (ary[i] == value) {
+            return i
+          }
+        }
+      } else if (fromIndex < 0) {
+        var afterNum = Math.abs(fromIndex)
+        for (var i = ary.length - afterNum; i >= 0; i--) {
+          if (ary[i] == value) {
+            return i
+          }
+        }
+      }
+      return -1
+    },
+
+  lastIndexOf:
+    function (ary, value, fromIndex = ary.length) {
+      for (var i = fromIndex; i >= 0; i--) {
+        if (ary[i] === value) {
+          return i
+        }
+      }
+      return -1
+    },
+
+  initial:
+    function (ary) {
+      // return ary.slice(0, ary.length - 1)
+      var res = []
+      for (i = 0; i < ary.length - 1; i++) {
+        res.push(ary[i])
+      }
+      return res
+    },
+
+  intersection:
+    function (...ary) {
+
     }
-    return res
-  },
-
-
-
-
-
-
-
 
 
 
